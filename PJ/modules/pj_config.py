0601696bd8295e7c58e0dee25fe79c221921557e
tiengthettheky <32325354+tiengthettheky@users.noreply.github.com>
2022-07-19 08:46:48 +0700

    Update pj_config.py

--- PJ/modules/pj_config.py
+++ PJ/modules/pj_config.py
@@ -2,6 +2,6 @@
 ProjectConfig = {
     "ProjectName":"Hỗ trợ đầu tư",
     "Version":1.0,
-    "baseURL": "http://apihtdt.vienthongtayninh.vn:8083",
+    "baseURL": "",
     "frameMain" : 'frameMain',
 }
